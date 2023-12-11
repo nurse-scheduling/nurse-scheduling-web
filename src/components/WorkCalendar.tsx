6f6b328a6bba0ea857f41365154330411bf973a1
Hüseyin Emre Üğdül <drekipersonal@outlook.com>
2023-12-11 20:04:43 +0300

    fix FullCalendar timeZone, locale dayGridMonth added

--- src/components/WorkCalendar.tsx
+++ src/components/WorkCalendar.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import trLocale from '@fullcalendar/core/locales/tr';
 
 function WorkCalendar() {
     
     return (
         <FullCalendar
-            plugins={[dayGridPlugin]}
+            plugins={[dayGridPlugin,timeGridPlugin]}
             initialView="dayGridMonth"
             events={[
-                { title: 'Etkinlik 1', date: '2023-11-06' },
-                { title: 'Etkinlik 2', date: '2023-11-10' },
+                { title: 'Hüseyin Emre Üğdül', start: "2023-12-06T07:00:00", end: "2023-12-06T12:30:00" },
+                { title: 'Mert Batuhan Ünverdi', start: "2023-12-06T12:30:00", end: "2023-12-06T18:30:00" },
+                { title: 'Mert Batuhan Ünverdi', start: "2023-12-07T12:30:00", end: "2023-12-07T18:30:00" },
+                { title: 'Hüseyin Emre Üğdül', start: "2023-12-07T18:30:00", end: "2023-12-08T07:30:00" },
                 
             ]}
+            headerToolbar={{
+                left: "prev,next",
+                center: "title",
+                right: "timeGridWeek,dayGridMonth"
+            }}
+            locales={[trLocale]}
+            locale="tr"
+            timeZone='local'
         />
 
     )
