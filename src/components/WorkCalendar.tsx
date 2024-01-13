3bd6be467c2732898ffa70596a6f879e5339c014
Mert Batuhan ÜNVERD <mert.batuhan.unverdi@saritay.com>
2024-01-13 20:54:10 +0300

    changed dummy data values

--- src/components/WorkCalendar.tsx
+++ src/components/WorkCalendar.tsx
@@ -50,10 +50,10 @@ function WorkCalendar() {
                 plugins={[dayGridPlugin, timeGridPlugin]}
                 initialView="dayGridMonth"
                 events={[
-                    { title: 'Hüseyin Emre Üğdül', start: '2023-12-06T07:00:00', end: '2023-12-06T12:30:00' },
-                    { title: 'Mert Batuhan Ünverdi', start: '2023-12-06T12:30:00', end: '2023-12-06T18:30:00' },
-                    { title: 'Mert Batuhan Ünverdi', start: '2023-12-07T12:30:00', end: '2023-12-07T18:30:00' },
-                    { title: 'Hüseyin Emre Üğdül', start: '2023-12-07T18:30:00', end: '2023-12-08T07:30:00' },
+                    { title: 'Hüseyin Emre Üğdül', start: '2024-01-06T07:00:00', end: '2024-01-06T12:30:00' },
+                    { title: 'Mert Batuhan Ünverdi', start: '2024-01-06T12:30:00', end: '2024-01-06T18:30:00' },
+                    { title: 'Mert Batuhan Ünverdi', start: '2024-01-07T12:30:00', end: '2024-01-07T18:30:00' },
+                    { title: 'Hüseyin Emre Üğdül', start: '2024-01-07T18:30:00', end: '2024-01-08T07:30:00' },
                 ]}
                 headerToolbar={{
                     left: 'prev,next',
